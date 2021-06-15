0429cb8932cbe5c812a68d2ba7ee7603923ae7e0
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2021-06-15 12:44:02 +0100

    Create basic search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import "./styles/App.css";
 
 function App() {
   const [list, setList] = useState([]);
+  const [renderList, setRenderList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchMethod, setSearchMethod] = useState("");
+  const [searchTerms, setSearchTerms] = useState("Peter");
 
   useEffect(() => {
     fetchData();
@@ -19,13 +21,14 @@ function App() {
       })
       .then((data) => {
         setList(data);
+        setRenderList(data.slice(0, 10));
         setLoading(false);
       });
   };
 
-  const renderVids = (vidList, num) => {
-    const shortList = vidList.slice(0, num);
-    return shortList.map((el, ind) => {
+  const renderVids = (vidList) => {
+    // const shortList = vidList.slice(0, num);
+    return vidList.map((el, ind) => {
       return (
         <li key={"vid" + ind}>
           <a href={el.videoUrl}>
@@ -35,6 +38,24 @@ function App() {
         </li>
       );
     });
+  };
+
+  const searchByTerms = (list, terms) => {
+    const condition = new RegExp(terms);
+    const filteredList = list.filter((el) => {
+      let result =
+        condition.test(el.videoTitle) || condition.test(el.teacherName);
+      console.log(result);
+      return result;
+    });
+    console.log(filteredList);
+    setRenderList(filteredList);
+  };
+
+  const refreshAll = () => {
+    fetchData();
+    setSearchMethod("");
+    setSearchTerms("");
   };
 
   return (
@@ -53,23 +74,36 @@ function App() {
             </select>
             {searchMethod === "term" && (
               <>
-                <p>Enter your search term and then press refresh</p>
-                <input type="text"></input>
+                <p>
+                  Enter your search term and then press search. The input is
+                  case sensitive.
+                </p>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setSearchTerms(e.target.value);
+                  }}
+                ></input>
+                <button onClick={() => searchByTerms(list, searchTerms)}>
+                  Search
+                </button>
               </>
             )}
             {searchMethod === "tags" && (
               <>
                 <p>
                   Enter the tags that you would like to search for separated by
-                  commas. The top 20 rated video tutorials, which contain any of
-                  the tags provided will be returned.
+                  commas and then press search. The top 20 rated video
+                  tutorials, which contain any of the tags provided will be
+                  returned.
                 </p>
                 <input type="text"></input>
+                <button>Search</button>
               </>
             )}
 
-            <button onClick={fetchData}>Refresh</button>
-            <ul>{renderVids(list, 10)}</ul>
+            <button onClick={refreshAll}>Refresh</button>
+            <ul>{renderVids(renderList)}</ul>
           </>
         )}
       </header>
