2236efcdfdb27ba29acc85ef1988de2df12d5603
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2021-06-15 14:46:11 +0100

    Implement basic tag search function

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import "./styles/App.css";
 
 function App() {
   const [list, setList] = useState([]);
+  const [sortedList, setSortedList] = useState([]);
   const [renderList, setRenderList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchMethod, setSearchMethod] = useState("");
-  const [searchTerms, setSearchTerms] = useState("Peter");
+  const [searchTerms, setSearchTerms] = useState("");
+  // const [searchTags, setSearchTags] = useState([]);
 
   useEffect(() => {
     fetchData();
+    sortByRating(list);
   }, []);
 
   const fetchData = () => {
@@ -58,6 +61,60 @@ function App() {
     setSearchTerms("");
   };
 
+  const compareFunction = (a, b) => {
+    // function to sort an array of objects by averageUserRating
+    const ratingA = a.averageUserRating;
+    const ratingB = b.averageUserRating;
+
+    let comparison = 0;
+    if (ratingA > ratingB) {
+      comparison = 1;
+    } else if (ratingA < ratingB) {
+      comparison = -1;
+    }
+    return comparison * -1;
+  };
+
+  const sortByRating = (data) => {
+    // function to copy, sort and set the list as part of state
+    console.log("beginning sort");
+    const sorted = [...data].sort(compareFunction);
+    setSortedList(sorted);
+  };
+
+  const searchTags = () => {
+    // split the search input into an array with a max size of 5 elements and no empty elements
+    // also make sure that capitalization matches that of JSON regardless of user input
+    const tags = searchTerms
+      .split(" ")
+      .filter((el) => el.length !== 0)
+      .slice(0, 5)
+      .map((el) => {
+        const lower = el.toLowerCase();
+        return lower[0].toUpperCase() + lower.substring(1);
+      });
+
+    console.log(tags);
+
+    let result = [];
+
+    // use for loop to iterate over sorted list so that we can break out upon reaching 20
+    for (let i = 0; i < sortedList.length; i++) {
+      // create array with input tags and search tags combined
+      const combined = [...tags, ...sortedList[i].tags];
+      // compare combined array against a set to find duplicates (i.e. matches) and push into array
+      if (combined.length !== new Set(combined).size) {
+        console.log(sortedList[i]);
+        result.push(sortedList[i]);
+        if (result.length >= 20) {
+          console.log("res", result);
+          setRenderList(result);
+          i = sortedList.length;
+        }
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -92,13 +149,18 @@ function App() {
             {searchMethod === "tags" && (
               <>
                 <p>
-                  Enter the tags that you would like to search for separated by
-                  commas and then press search. The top 20 rated video
-                  tutorials, which contain any of the tags provided will be
-                  returned.
+                  Enter up to five tags that you would like to search for
+                  separated by spaces and then press search. The top 20 rated
+                  video tutorials, which contain any of the tags provided will
+                  be returned.
                 </p>
-                <input type="text"></input>
-                <button>Search</button>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setSearchTerms(e.target.value);
+                  }}
+                ></input>
+                <button onClick={searchTags}>Search Tags</button>
               </>
             )}
 
